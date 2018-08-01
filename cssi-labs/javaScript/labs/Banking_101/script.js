a8964de41caf0a7bb64a1ec9be16e8154417887a
Shibaruto <andrwe.daved@gmail.com>
2018-08-01 15:09:42 -0400

    15:09

--- cssi-labs/javaScript/labs/Banking_101/script.js
+++ cssi-labs/javaScript/labs/Banking_101/script.js
@@ -15,24 +15,31 @@
 let customer_name;
 let balance;
 
-function openAccount(name,balance = 100){
-  mybalance = balance;
+function openAccount(name,mybalance = 0){
+ balance = mybalance;
   // Set the value for customer_name equal to name below
 customer_name = name;
-return name + " Has opened a new account with a balance of $" + mybalance;
+return name + " Has opened a new account with a balance of $" + balance;
 //write the statment you need to return here
 }
 
 function deposit(value){
-  // update the value of balance
-  //return the correct statement
+  balance = balance + value;
+  let msg = `${customer_name} has deposited ${value}. ${customer_name}'s total balance is ${balance}`;
+  return msg;
 }
 
-function withdraw(/*argument here*/){
-  //update the value of balance
-  //return the correct statement
-}
 
-// Write your script below
+function withdraw(amount){
+  if (amount > balance){
+    let difference = amount - balance;
+    return `Sorry, ${customer_name} you do not have enough money in your account. You need ${difference} more dollars.`;
+  }
+  balance = balance - amount;
+  let sg = `${customer_name} has withdraw ${amount}. ${customer_name} has ${balance} remaining.`;
+  return sg;
+}
 console.log("script is running...");
-console.log(openAccount("James","1000"));
+console.log(openAccount("James",300));
+console.log(deposit(50));
+console.log(withdraw(500));
